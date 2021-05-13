d6c570ecb01b947a6ea18f40416431d6d91e2fd5
alexandrejflopes <alxlopes@live.com.pt>
2021-05-13 19:17:10 +0100

    Update index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-
 import App from './App/index';
+import config from './config';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
-import config from './config';
+
 
 const store = createStore(reducer);
 
