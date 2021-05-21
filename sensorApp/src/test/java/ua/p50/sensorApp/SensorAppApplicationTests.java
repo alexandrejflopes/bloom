1067658ced4727be1f86819e0bf60b43c1e6b378
Alina <alina-yanchuk02@outlook.pt>
2021-05-21 18:35:34 +0100

    starting sensorsApp

---
+++ sensorApp/src/test/java/ua/p50/sensorApp/SensorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.p50.sensorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
