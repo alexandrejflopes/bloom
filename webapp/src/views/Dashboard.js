7d1af0b12b186fa8dcf1f359f3411dcb2b5ebc71
Sofia Moniz <sofiamoniz@ua.pt>
2021-05-30 11:50:52 +0100

    estufa não estática

--- webapp/src/views/Dashboard.js
+++ webapp/src/views/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Row, Col, Card} from 'react-bootstrap';
+import {Row, Col, Card, Button} from 'react-bootstrap';
 
 import Aux from "../hoc/_Aux";
-import "../assets/css/style.css"
+import "../assets/css/estufa.css"
 
 class Dashboard extends React.Component {
     render() {
@@ -17,9 +17,29 @@ class Dashboard extends React.Component {
                                 <span className="d-block m-t-5">Humidade, temperatura e CO2</span>
                             </Card.Header>
                             <Card.Body>
-                                <img className="estufa_dash" src={require('../assets/images/estufa/estufa.png')} />
+                                {/* Versão estática */}
+                                {/*<img className="estufa_dash" src={require('../assets/images/estufa/estufa.png')} />*/}
+                                
+                                <div className="estufa_div">
+                                    <img alt="estufa imgs" src={require('../assets/images/estufa/fundo_estufa.png')} className="fundo_estufa"/>
+                                    <div className="fundo_esquerda"></div>
+                                    <img alt="temperature" src={require('../assets/images/estufa/temperatura.png')} className="temperatura_esquerda"/>
+                                    <p className="valor_temp_esquerda">22ºC</p>
+                                    <img alt="co2" src={require('../assets/images/estufa/co2.png')} className="c02_esquerda"/>
+                                    <p className="valor_c02_esquerda">980 ppm</p>
+                                    <img alt="tabuleiro img" src={require('../assets/images/estufa/tabuleiro_branco.png')} className="tabuleiro1"/>
+                                    <div className="humidade_tab1_bg"></div>
+                                    <img alt="tabuleiro2 img" src={require('../assets/images/estufa/tabuleiro_branco.png')} className="tabuleiro2"/>
+                                    <div className="humidade_tab2_bg"></div>
+                                    <img alt="tabuleiro3 img" src={require('../assets/images/estufa/tabuleiro_branco.png')} className="tabuleiro3"/>
+                                    <div className="humidade_tab3_bg"></div>
+                                    <div className="fundo_direita"></div>
+                                    <img alt="temperature" src={require('../assets/images/estufa/temperatura.png')} className="temperatura_direita"/>
+                                    <p className="valor_temp_direita">22ºC</p>
+                                    <img alt="co2" src={require('../assets/images/estufa/co2.png')} className="c02_direita"/>
+                                    <p className="valor_c02_direita">980 ppm</p>
+                                </div>
                             </Card.Body>
-                            <p>quando a página de settings estiver feita, vão aparecer aqui botões caso o modo escolhido seja manual</p>
                         </Card>
                         
                     </Col>
