841b040e15aae5a30612c2a029a36c2a2f0a9c87
Sofia Moniz <sofiamoniz@ua.pt>
2021-05-30 14:52:14 +0100

    settings page

--- webapp/src/views/Settings.js
+++ webapp/src/views/Settings.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Row, Col, Card, Table} from 'react-bootstrap';
+import {Row, Col, Card, Form, Button, InputGroup, FormControl, DropdownButton, Dropdown, Tab, Tabs} from 'react-bootstrap';
 
 import Aux from "../hoc/_Aux";
+import "../assets/css/settings.css"
 
 class Settings extends React.Component {
     render() {
@@ -11,118 +12,75 @@ class Settings extends React.Component {
                     <Col>
                         <Card>
                             <Card.Header>
-                                <Card.Title as="h5">Basic Table</Card.Title>
-                                <span className="d-block m-t-5">use bootstrap <code>Table</code> component</span>
+                                <Card.Title as="h5">Configurações</Card.Title>
                             </Card.Header>
                             <Card.Body>
-                                <Table responsive>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    </tbody>
-                                </Table>
+                                <Row>
+                                    <Col>
+                                        <h5>Modo</h5>
+                                        <hr/>
+                                        <Form inline>
+                                            <Form.Group className="mb-2">
+                                                <Form.Label>Controlo de ambiente:</Form.Label>
+                                            </Form.Group>
+                                            <Form.Group className="mb-2 mr-5 modo_dropdown">
+                                                <Form.Group controlId="exampleForm.ControlSelect1">
+                                                    <Form.Control as="select">
+                                                        <option>Automático</option>
+                                                        <option>Manual</option>
+                                                    </Form.Control>
+                                                </Form.Group>
+                                            </Form.Group>
+                                        </Form>
+                                        <div className="parametro_tabs">
+                                            <h5>Limites aceitáveis</h5>
+                                            <hr/>
+                                            <Tabs defaultActiveKey="humidade" >
+                                                <Tab eventKey="humidade" title="Humidade">
+                                                    <Col md={6}>
+                                                        <Form.Group controlId="limiteMinimoHumidade">
+                                                            <Form.Label>Limite Mínimo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                        </Form.Group>
+                                                        <Form.Group controlId="limiteMaximoHumidade">
+                                                            <Form.Label>Limite Máximo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                        </Form.Group>
+                                                    </Col>
+                                                </Tab>
+                                                <Tab eventKey="temperatura" title="Temperatura">
+                                                    <Col md={6}>
+                                                        <Form.Group controlId="limiteMinimoTemperatura">
+                                                            <Form.Label>Limite Mínimo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                        </Form.Group>
+                                                        <Form.Group controlId="limiteMaximoTemperatura">
+                                                            <Form.Label>Limite Máximo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                        </Form.Group>
+                                                    </Col>
+                                                </Tab>
+                                                <Tab eventKey="co2" title="CO2">
+                                                    <Col md={6}>
+                                                        <Form.Group controlId="limiteMinimoCo2">
+                                                            <Form.Label>Limite Mínimo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                        </Form.Group>
+                                                        <Form.Group controlId="limiteMaximoCo2">
+                                                            <Form.Label>Limite Máximo</Form.Label>
+                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                        </Form.Group>
+                                                    </Col>
+                                                </Tab>
+                                            </Tabs>
+                                        </div>
+                                        <Button variant="outline-dark" className="guardar_bt">Guardar</Button>
+                                    </Col>
+                                </Row>
+                                
                             </Card.Body>
                         </Card>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Hover Table</Card.Title>
-                                <span className="d-block m-t-5">use props <code>hover</code> with <code>Table</code> component</span>
-                            </Card.Header>
-                            <Card.Body>
-                                <Table responsive hover>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Striped Table</Card.Title>
-                                <span className="d-block m-t-5">use props <code>striped</code> with <code>Table</code> component</span>
-                            </Card.Header>
-                            <Card.Body>
-                                <Table striped responsive>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
+                        
                     </Col>
                 </Row>
             </Aux>
