65fe9b194b652ad0a9b425a8489c8ed40691c799
Sofia Moniz <sofiamoniz@ua.pt>
2021-06-03 10:22:23 +0100

    username

--- webapp/src/views/Settings.js
+++ webapp/src/views/Settings.js
@@ -4,71 +4,71 @@ import {Row, Col, Card, Form, Button, InputGroup, FormControl, DropdownButton, D
 import Aux from "../hoc/_Aux";
 import "../assets/css/settings.css"
 
-class Settings extends React.Component {
-    render() {
-        return (
-            <Aux>
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Configurações</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <Row>
-                                    <Col>
-                                            <h6>Estufa de  </h6>
-                                            <hr/>
-                                            <Tabs defaultActiveKey="humidade" >
-                                                <Tab eventKey="humidade" title="Humidade">
-                                                    <Col md={6}>
-                                                        <Form.Group controlId="limiteMinimoHumidade">
-                                                            <Form.Label>Limite Mínimo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
-                                                        </Form.Group>
-                                                        <Form.Group controlId="limiteMaximoHumidade">
-                                                            <Form.Label>Limite Máximo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
-                                                        </Form.Group>
-                                                    </Col>
-                                                </Tab>
-                                                <Tab eventKey="temperatura" title="Temperatura">
-                                                    <Col md={6}>
-                                                        <Form.Group controlId="limiteMinimoTemperatura">
-                                                            <Form.Label>Limite Mínimo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
-                                                        </Form.Group>
-                                                        <Form.Group controlId="limiteMaximoTemperatura">
-                                                            <Form.Label>Limite Máximo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
-                                                        </Form.Group>
-                                                    </Col>
-                                                </Tab>
-                                                <Tab eventKey="co2" title="CO2">
-                                                    <Col md={6}>
-                                                        <Form.Group controlId="limiteMinimoCo2">
-                                                            <Form.Label>Limite Mínimo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor mínimo aceitável" />
-                                                        </Form.Group>
-                                                        <Form.Group controlId="limiteMaximoCo2">
-                                                            <Form.Label>Limite Máximo</Form.Label>
-                                                            <Form.Control type="text" placeholder="Valor máximo aceitável" />
-                                                        </Form.Group>
-                                                    </Col>
-                                                </Tab>
-                                            </Tabs>
-                                        <Button variant="outline-dark" className="guardar_bt">Guardar</Button>
-                                    </Col>
-                                </Row>
-                                
-                            </Card.Body>
-                        </Card>
-                        
-                    </Col>
-                </Row>
-            </Aux>
-        );
-    }
+function Settings() {
+    const[email, setEmail]=React.useState(localStorage.getItem('email'));
+    console.log("email ", email);
+    return (
+        <Aux>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Header>
+                            <Card.Title as="h5">Configurações</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col>
+                                        <h6>Estufa de {email}</h6>
+                                        <hr/>
+                                        <Tabs defaultActiveKey="humidade" >
+                                            <Tab eventKey="humidade" title="Humidade">
+                                                <Col md={6}>
+                                                    <Form.Group controlId="limiteMinimoHumidade">
+                                                        <Form.Label>Limite Mínimo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                    </Form.Group>
+                                                    <Form.Group controlId="limiteMaximoHumidade">
+                                                        <Form.Label>Limite Máximo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Tab>
+                                            <Tab eventKey="temperatura" title="Temperatura">
+                                                <Col md={6}>
+                                                    <Form.Group controlId="limiteMinimoTemperatura">
+                                                        <Form.Label>Limite Mínimo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                    </Form.Group>
+                                                    <Form.Group controlId="limiteMaximoTemperatura">
+                                                        <Form.Label>Limite Máximo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Tab>
+                                            <Tab eventKey="co2" title="CO2">
+                                                <Col md={6}>
+                                                    <Form.Group controlId="limiteMinimoCo2">
+                                                        <Form.Label>Limite Mínimo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor mínimo aceitável" />
+                                                    </Form.Group>
+                                                    <Form.Group controlId="limiteMaximoCo2">
+                                                        <Form.Label>Limite Máximo</Form.Label>
+                                                        <Form.Control type="text" placeholder="Valor máximo aceitável" />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Tab>
+                                        </Tabs>
+                                    <Button variant="outline-dark" className="guardar_bt">Guardar</Button>
+                                </Col>
+                            </Row>
+                            
+                        </Card.Body>
+                    </Card>
+                    
+                </Col>
+            </Row>
+        </Aux>
+    );
 }
 
 export default Settings;
