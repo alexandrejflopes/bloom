692fc3e4bcdd0464d52e0d611a4f0184b4a4ab49
edgargsm <39306840+edgargsm@users.noreply.github.com>
2021-06-03 10:47:50 +0100

    Update Utils.java

--- sensorsApp/src/main/java/ua/p50/sensorsApp/utils/Utils.java
+++ sensorsApp/src/main/java/ua/p50/sensorsApp/utils/Utils.java
@@ -3,6 +3,6 @@ package ua.p50.sensorsApp.utils;
 public class Utils {
     
     //public static final String IP = "localhost";
-    public static final String IP = "192.168.160.87";
+    public static final String IP = "192.168.160.18";
 
 }
