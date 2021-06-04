0b33da33146d98bbe694445f87c8d37400417e19
Sofia Moniz <sofiamoniz@ua.pt>
2021-06-04 15:53:33 +0100

    new alert

--- webapp/src/views/Settings.js
+++ webapp/src/views/Settings.js
@@ -10,26 +10,69 @@ function Settings() {
     const[maxTemp, setMaxTemp] = React.useState(30);
     const[maxHum, setMaxHum] = React.useState(700);
     const[minHum, setMinHum] = React.useState(0);
+    const[sucesso,setSucesso] = React.useState(false);
+    const[limitesErro,setLimitesErro] = React.useState(false);
+    const[tempErro,setTempErro] = React.useState(false);
+    const[humErro,setHumErro] = React.useState(false);
+
+
 
     const guardarLimites = () => {
 
-        if(minTemp>maxTemp || minHum>maxHum){
-            alert("Os limites mínimos não podem ser superiores aos limites máximos!")
-        }
-        else if(maxTemp<minTemp || maxHum<minHum){
-            alert("Os limites máximos não podem ser inferiores aos limites máximos!")
+        if(minTemp>maxTemp || minHum>maxHum || maxTemp<minTemp || maxHum<minHum){
+            //alert("Os limites mínimos não podem ser superiores aos limites máximos!")
+            setLimitesErro(true);
+            setSucesso(false);
         }
         else if( maxTemp>30 || minTemp<0 ){
-            alert("A temperatura deve encontrar-se entre 0ºC e 30ºC.")
+            //alert("A temperatura deve encontrar-se entre 0ºC e 30ºC.")
+            setTempErro(true);
+            setSucesso(false);
         }
         else if(maxHum>700 || minHum<0){
-            alert("A humidade deve encontrar-se entre 0ppm e 700ppm.")
+            //alert("A humidade deve encontrar-se entre 0ppm e 700ppm.")
+            setHumErro(true);
+            setSucesso(false);
         }
         else{
-            alert("Sucesso! Novos limites de humidade e temperatura salvos.")
+            //alert("Sucesso! Novos limites de humidade e temperatura salvos.")
+            setSucesso(true);
         }
         
     }
+
+    const getAlert = () => {
+        if(sucesso){
+            return(
+                <div className="alert alert-success" role="alert" id="sucesso_alert">
+                    Sucesso! Novos limites de humidade e temperatura salvos.
+                </div>
+            )
+        }
+        else if(limitesErro){
+            return(
+                <div className="alert alert-danger" role="alert" id="limites_alert">
+                    Os limites mínimos não podem ser superiores aos limites máximos!
+                </div>
+            )
+        } 
+        else if(tempErro){
+            return(
+                <div className="alert alert-danger" role="alert" id="temp_alert">
+                    A temperatura deve encontrar-se entre 0ºC e 30ºC.
+                </div>
+            )
+        }        
+        else if(humErro){
+            return(
+                <div className="alert alert-danger" role="alert" id="hum_alert">
+                    A humidade deve encontrar-se entre 0ppm e 700ppm.
+                </div>
+            )
+        } 
+        
+    }
+
 
     return (
         <Aux>
@@ -49,28 +92,31 @@ function Settings() {
                                                 <Col md={6}>
                                                     <Form.Group controlId="limiteMinimoHumidade">
                                                         <Form.Label>Limite Mínimo</Form.Label>
-                                                        <Form.Control type="number"  placeholder="Valor mínimo aceitável" value={minHum} onChange={e => setMinHum(e.target.value)} />
+                                                        <Form.Control type="number" id="min_hum"  placeholder="Valor mínimo aceitável" value={minHum} onChange={e => setMinHum(e.target.value)} />
                                                     </Form.Group>
                                                     <Form.Group controlId="limiteMaximoHumidade">
                                                         <Form.Label>Limite Máximo</Form.Label>
-                                                        <Form.Control type="number" placeholder="Valor máximo aceitável" value={maxHum} onChange={e => setMaxHum(e.target.value)}  />
+                                                        <Form.Control type="number" id="max_hum" placeholder="Valor máximo aceitável" value={maxHum} onChange={e => setMaxHum(e.target.value)}  />
                                                     </Form.Group>
                                                 </Col>
                                             </Tab>
-                                            <Tab eventKey="temperatura" title="Temperatura">
+                                            <Tab id="temperature_tab" eventKey="temperatura" title="Temperatura">
                                                 <Col md={6}>
                                                     <Form.Group controlId="limiteMinimoTemperatura">
                                                         <Form.Label>Limite Mínimo</Form.Label>
-                                                        <Form.Control type="number" placeholder="Valor mínimo aceitável" value={minTemp} onChange={e => setMinTemp(e.target.value)} />
+                                                        <Form.Control type="number" id="min_temp" placeholder="Valor mínimo aceitável" value={minTemp} onChange={e => setMinTemp(e.target.value)} />
                                                     </Form.Group>
                                                     <Form.Group controlId="limiteMaximoTemperatura">
                                                         <Form.Label>Limite Máximo</Form.Label>
-                                                        <Form.Control type="number" placeholder="Valor máximo aceitável" value={maxTemp} onChange={e => setMaxTemp(e.target.value)}  />
+                                                        <Form.Control type="number" id="max_temp" placeholder="Valor máximo aceitável" value={maxTemp} onChange={e => setMaxTemp(e.target.value)}  />
                                                     </Form.Group>
                                                 </Col>
                                             </Tab>
                                         </Tabs>
-                                    <Button variant="outline-dark" className="guardar_bt" onClick={() => guardarLimites()}>Guardar</Button>
+                                        { 
+                                            getAlert()
+                                        }
+                                    <Button variant="outline-dark" className="guardar_bt" id="guardar_bt" onClick={() => guardarLimites()}>Guardar</Button>
                                 </Col>
                             </Row>
                             
