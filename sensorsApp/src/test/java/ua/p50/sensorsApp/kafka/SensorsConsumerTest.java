7a952df4855da7707dfe806fd9bee99c31c78479
edgargsm <39306840+edgargsm@users.noreply.github.com>
2021-06-13 20:39:53 +0100

    Update SensorsConsumerTest.java

--- sensorsApp/src/test/java/ua/p50/sensorsApp/kafka/SensorsConsumerTest.java
+++ sensorsApp/src/test/java/ua/p50/sensorsApp/kafka/SensorsConsumerTest.java
@@ -20,7 +20,7 @@ import ua.p50.sensorsApp.models.Sensor;
 
 @SpringBootTest
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" }, topics = { "esp50-sensors-temperature",  "esp50-sensors-humidity", "esp50-sensors-co2"})
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class SensorsConsumerTest {
 
   @Autowired
